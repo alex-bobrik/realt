bed80c3c82d08e75b0a4b6b8d62973e802a10acc
alex-bobrik <sasha.bobrik@inbox.ru>
2020-06-19 16:32:39 +0300

    Add styles

--- resources/views/houses/index.blade.php
+++ resources/views/houses/index.blade.php
@@ -11,9 +11,54 @@
 
     <!-- Styles -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+    <style>
+        body {
+            background: #F8F8F8;
+
+        }
+        .container {
+            margin-top: 80px;
+        }
+        .house {
+            padding: 20px;
+            border-radius: 15px;
+            margin: 20px;
+            display: flex;
+
+            /*  Shadow  */
+            -webkit-box-shadow: 5px -1px 43px -19px rgba(10,9,10,0.83);
+            -moz-box-shadow: 5px -1px 43px -19px rgba(10,9,10,0.83);
+            box-shadow: 5px -1px 43px -19px rgba(10,9,10,0.83);
+        }
+        .house-right {
+            margin-right: 100px;
+            width: 10%;
+        }
+        .house-left {
+        }
+        .button-search {
+            width: 100%;
+        }
+    </style>
+
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <a class="navbar-brand" href="#">Realt.by Parser</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('/houses')}}">Search for houses</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 <div class="container">
     <div id="realt"></div>
@@ -53,10 +98,23 @@
                 <div>
                     <Form housesMethod = {this.gettingHouses}/>
                     <div>
-                        {houses.map(hit =>
+                        {houses.map(house =>
                             <div className="house">
-                                <img src={hit.image_link} alt=""/>
-                                <p>{hit.title}</p>
+                                <div className="house-right">
+                                    <img src={house.image_link} alt="image"/>
+                                    Updated: {house.updated}
+
+                                </div>
+                                <div className="house-left">
+                                    <p className="house-updated">
+                                        <p>Title: {house.title}</p>
+                                        <p>Description: {house.description}</p>
+                                        <p>Rooms amount: {house.rooms}</p>
+                                        <p>Contacts: {house.contacts}</p>
+                                        <p>Price: {house.price_per_day ? house.price_per_day + ' per day' : 'Contact me for price!'}</p>
+                                    </p>
+                                </div>
+
                             </div>
                         )}
                     </div>
@@ -88,17 +146,14 @@
                         <label htmlFor="priceTo">Price to</label>
                         <input type="number" name="priceTo" className="form-control" min="0" required/>
                     </div>
-                        <button className="btn btn-primary">Search</button>
+                        <button className="btn btn-primary button-search">Search</button>
                 </form>
             )
         }
     }
 
     ReactDOM.render(<House />, document.getElementById('realt'));
-
-
 </script>
-
 
 </body>
 </html>
